fca5495af72ade4b413c1bf8f8fbe0b8951de320
LENOVO-PC\kaibin <kayu6150@uni.sydney.edu.au>
2016-10-25 09:26:51 +1100

    EXTEND TEST CASE FOR PROJECT MANAGER

--- SpringMvc/src/test/resources/com/unisyd_elec5619/springmvc/projecttests/projectManagerTest.java
+++ SpringMvc/src/test/resources/com/unisyd_elec5619/springmvc/projecttests/projectManagerTest.java
@@ -16,11 +16,19 @@ public class projectManagerTest extends TestCase{
 	private static int PRODUCT_COUNT = 2;
 	
 	private static Double PROJECT_BUGET = new Double(20.50);
-	private static String PROJECT_DESCRIPTION = "Chair";
+	private static String PROJECT_DESCRIPTION = "my project 1";
+	private static String PROJECT_NAME = "Project Name 1";
+	private static Date PROJECT_START_DATE = new Date(123);
+	private static Date PROJECT_DEADLINE_DATE = new Date(321);
+	private static String PROJECT_MANAGER = "mu project 1 manager";
 	
-	private static String PROJECT2_DESCRIPTION = "Table";
+	private static String PROJECT2_DESCRIPTION = "my project 2";
 	private static Double PROJECT2_PRICE = new Double(150.10);
-	
+	private static String PROJECT2_NAME = "Project Name 2";
+	private static Date PROJECT2_START_DATE = new Date(1234);
+	private static Date PROJECT2_DEADLINE_DATE = new Date(4321);
+	private static String PROJECT2_MANAGER = "mu project 2 manager";
+
 	private static int POSITIVE_PRICE_INCREASE = 10;
 	
 	protected void setUp() throws Exception{
@@ -30,15 +38,19 @@ public class projectManagerTest extends TestCase{
 		Project project = new Project();
 		project.setDescription(PROJECT_DESCRIPTION);
 		project.setPrice(PROJECT_BUGET);
-		project.setName("project title");
-		project.setProjectStartDate(new Date());
-		project.setProjectDeadline(new Date());
-		project.setProjectManager("New project Manager");
+		project.setName(PROJECT_NAME);
+		project.setProjectStartDate(PROJECT_START_DATE);
+		project.setProjectDeadline(PROJECT_DEADLINE_DATE);
+		project.setProjectManager(PROJECT_MANAGER);
 		projects.add(project);
 		
 		project = new Project();
-		project.setDescription("Table");
+		project.setName(PROJECT2_NAME);
+		project.setDescription(PROJECT2_DESCRIPTION);
 		project.setPrice(PROJECT2_PRICE);
+		project.setProjectStartDate(PROJECT2_START_DATE);
+		project.setProjectDeadline(PROJECT2_DEADLINE_DATE);
+		project.setProjectManager(PROJECT_MANAGER);
 		projects.add(project);
 		
 		projectManager.setProjects(projects);
@@ -49,18 +61,54 @@ public class projectManagerTest extends TestCase{
 		assertNull(projectManager.getProjects());
 	}
 	
-	public void testGetProducts(){
-		List<Project> products = projectManager.getProjects();
-		assertNotNull(products);
+	public void testGetProjectsDescription(){
+		List<Project> projects = projectManager.getProjects();
+		assertNotNull(projects);
 		assertEquals(PRODUCT_COUNT, projectManager.getProjects().size());
 		
-		Project product = products.get(0);
-		assertEquals(PROJECT_DESCRIPTION, product.getDescription());
-		assertEquals(PROJECT_BUGET, product.getPrice());
+		Project project = projects.get(0);
+		assertEquals(PROJECT_DESCRIPTION, project.getDescription());
+		assertEquals(PROJECT_BUGET, project.getPrice());
 		
-		product = products.get(1);
-		assertEquals(PROJECT2_DESCRIPTION, product.getDescription());
-		assertEquals(PROJECT2_PRICE, product.getPrice());
+		project = projects.get(1);
+		assertEquals(PROJECT2_DESCRIPTION, project.getDescription());
+		assertEquals(PROJECT2_PRICE, project.getPrice());
+	}
+	public void testGetProjectsName(){
+		List<Project> projects = projectManager.getProjects();
+		assertNotNull(projects);		
+		Project project = projects.get(0);
+		assertEquals(PROJECT_NAME, project.getName());
+		
+		project = projects.get(1);
+		assertEquals(PROJECT2_NAME, project.getName());
+	}
+	public void testGetProjectsStartDate(){
+		List<Project> projects = projectManager.getProjects();
+		assertNotNull(projects);		
+		Project project = projects.get(0);
+		assertEquals(PROJECT_START_DATE, project.getProjectStartDate());
+		
+		project = projects.get(1);
+		assertEquals(PROJECT2_START_DATE, project.getProjectStartDate());
+	}
+	public void testGetProjectsDeadline(){
+		List<Project> projects = projectManager.getProjects();
+		assertNotNull(projects);		
+		Project project = projects.get(0);
+		assertEquals(PROJECT_DEADLINE_DATE, project.getProjectDeadline());
+		
+		project = projects.get(1);
+		assertEquals(PROJECT2_DEADLINE_DATE, project.getProjectDeadline());
+	}
+	public void testGetProjectsManager(){
+		List<Project> projects = projectManager.getProjects();
+		assertNotNull(projects);		
+		Project project = projects.get(0);
+		assertEquals(PROJECT_MANAGER, project.getProjectManager());
+		
+		project = projects.get(1);
+		assertEquals(PROJECT2_MANAGER, project.getProjectManager());
 	}
 	
 	public void testIncreasePriceWithNullListOfProducts() {
